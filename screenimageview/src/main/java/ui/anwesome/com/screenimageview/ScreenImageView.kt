a7ef925733601d1e532eeef00de0b806afb0fadc
Anwesh43 <anweshthecool0@gmail.com>
2018-01-17 01:47:20 +0530

    adding ScreenImage

--- screenimageview/src/main/java/ui/anwesome/com/screenimageview/ScreenImageView.kt
+++ screenimageview/src/main/java/ui/anwesome/com/screenimageview/ScreenImageView.kt
@@ -6,7 +6,7 @@ package ui.anwesome.com.screenimageview
 import android.graphics.*
 import android.content.*
 import android.view.*
-class ScreenImageView(ctx:Context):View(ctx) {
+class ScreenImageView(ctx:Context,var bitmap:Bitmap):View(ctx) {
     val paint = Paint(Paint.ANTI_ALIAS_FLAG)
     override fun onDraw(canvas:Canvas) {
 
@@ -18,5 +18,37 @@ class ScreenImageView(ctx:Context):View(ctx) {
             }
         }
         return true
+    }
+    data class ScreenImage(var bitmap:Bitmap,var x:Float,var y:Float) {
+        fun draw(canvas:Canvas,paint:Paint) {
+            val w = bitmap.width.toFloat()
+            val h = bitmap.height.toFloat()
+            paint.strokeWidth = Math.min(w,h)/50
+            paint.strokeCap = Paint.Cap.ROUND
+            canvas.save()
+            canvas.translate(x,y)
+            canvas.save()
+            val path = Path()
+            path.addRect(RectF(-w/2,-h/2,w/2,h/2),Path.Direction.CW)
+            canvas.clipPath(path)
+            canvas.drawBitmap(bitmap,-w/2,-h/2,paint)
+            canvas.restore()
+            paint.color = Color.WHITE
+            for(i in 0..1) {
+                canvas.save()
+                val px = w/10+(w/2-w/10)
+                val sx = px*(i*2-1)
+                canvas.drawLine(sx,0f,sx-w/10,-w/10,paint)
+                canvas.drawLine(sx,0f,sx-w/10,w/10,paint)
+                canvas.restore()
+            }
+            canvas.restore()
+        }
+        fun update(stopcb:(Float) -> Unit) {
+
+        }
+        fun startUpdating(startcb:() -> Unit) {
+
+        }
     }
 }
