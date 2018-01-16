58e037b3517dcf9c5be7aeb7fffaaa1b281c6b2e
Anwesh43 <anweshthecool0@gmail.com>
2018-01-17 01:52:18 +0530

    adding ScreenImageState

--- screenimageview/src/main/java/ui/anwesome/com/screenimageview/ScreenImageView.kt
+++ screenimageview/src/main/java/ui/anwesome/com/screenimageview/ScreenImageView.kt
@@ -51,4 +51,19 @@ class ScreenImageView(ctx:Context,var bitmap:Bitmap):View(ctx) {
 
         }
     }
+    data class ScreenImageState(var scale:Float = 0f,var dir:Float = 0f,var prevScale:Float = 0f) {
+        fun update(stopcb:(Float)->Unit) {
+            scale += 0.1f*dir
+            if(Math.abs(scale - prevScale) > 1) {
+                scale = prevScale + dir
+                dir = 0f
+                prevScale = scale
+                stopcb(scale)
+            }
+        }
+        fun startUpdating(startcb:()->Unit) {
+            dir = 1 - 2*scale
+            startcb()
+        }
+    }
 }
