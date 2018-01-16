947ee746143885bc45ffaa4a8d5533ea181ba628
Anwesh43 <anweshthecool0@gmail.com>
2018-01-17 02:30:12 +0530

    using ScreenImageView in Activity

--- app/src/main/java/ui/anwesome/com/kotlinscreenimageview/MainActivity.kt
+++ app/src/main/java/ui/anwesome/com/kotlinscreenimageview/MainActivity.kt
@@ -1,12 +1,14 @@
 package ui.anwesome.com.kotlinscreenimageview
 
+import android.graphics.BitmapFactory
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import ui.anwesome.com.screenimageview.ScreenImageView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        ScreenImageView.create(this,BitmapFactory.decodeResource(resources,R.drawable.nature))
     }
 }
